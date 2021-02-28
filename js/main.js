4e4a48d6c7fb00cf912798b30012553c80bae1a1
denisIgnatow <51458687+denisIgnatow@users.noreply.github.com>
2021-02-28 17:53:44 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,3 +2,5 @@ jQuery('.menuicon').on('click', function() {
   jQuery(this).toggleClass('active')
   jQuery('.header__contacts').toggleClass('active')
 })
+
+jQuery('.form-inv__num').mask('+7 (999) 999-99-99')
